ee5d33101add3c6afc9d52af4414232e21781f86
mian-adeel <adeelmian83@gmail.com>
2023-04-21 14:46:22 -0400

    files updated

---
+++ index.js
@@ -0,0 +1,63 @@
+// Declare variables for the two numbers
+// Calculate the sum of the two numbers
+// Use if/else if/else statements to print the corresponding message based on the sum
+
+
+// let num1 = -5;
+// let num2 = 0;
+// let sum = num1 + num2;
+
+// if (sum > 100) {
+//   console.log(sum + " is greater than 100");
+// } else if (sum < -1000) {
+//   console.log(sum + " is less than -1000");
+// } else if (sum > 0) {
+//   console.log(sum + " is greater than 0");
+// } else if (sum === 0) {
+//   console.log(sum + " is equal to 0");
+// } else {
+//   console.log(sum + " is a negative number");
+// }
+
+
+
+
+// Declare a variable and assign it a truthy value
+// Check if the value is truthy using an if/else statement
+// Print the assigned value 
+
+
+// let value = undefined;
+
+// if (typeof(value) === "string") {
+//   console.log(true);
+// } else if (value === false) {
+//   console.log("The boolean value false is falsy");
+// } else if (value === null) {
+//   console.log("The null value is falsy");
+// } else if (value === undefined) {
+//   console.log("undefined is falsy");
+// } else if (value === 0) {
+//   console.log("The number 0 is falsy (the only falsy number)");
+// } else if (value === "") {
+//   console.log("The empty string is falsy (the only falsy string)");
+// }
+
+
+
+
+
+// Declare variables for the two parameters
+// Check if both parameters are greater or equal to 5 using an if/else statement
+// If both parameters are greater or equal to 5, print "true" to the console
+// If one or both parameters are less than 5, print "false" to the console
+
+
+// let num1 = 6;
+//   let num2 =4;
+
+//   if (num1 >= 5 && num2 >= 5) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
